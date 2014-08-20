0a270c04125a70841ab0132619b7dfda282ad318
xiejiati <xiejiati2070@163.com>
2014-08-20 15:26:12 +0800

    oil

--- Handler.py
+++ Handler.py
@@ -6,42 +6,34 @@ from translator import *
 
 class handler:
     def product_each(self, name, data):
+        single, double = record_array_producer()
+        iter(compute_product_value, data, name, single, double)
         product_value = {}
-        single = []
-        single.append(0)
-        double = []
-        double.append(0)
-        for v in data.items():
-            compute_product_value(name, v, '第一趟', single, double)
-            if v.get('回头货'):
-                compute_product_value(name, v, '回头货', single, double)
         product_value['个人'] = single[0]
         product_value['两人'] = double[0]
         return product_value
 
     def miles_each(self, name, data):
-        single = heavy_dict()
-        double = heavy_dict()
-        for v in data.values():
-            compute_miles(name, v, '第一趟', single, double)
-            if v.get('回头货'):
-                compute_miles(name, v, '回头货', single, double)
+        single = heavy_dict_producer()
+        double = heavy_dict_producer()
+        iter(compute_miles, data, name, single, double)
         miles = {}
         miles['个人'] = single
         miles['两人'] = double
         return miles
 
     def oil_each(self, name, data):
-
-
-
-
-
-
-
+        single, double = record_array_producer()
+        iter(comput_oil, data, name, single, double)
+        oil = {}
+        oil['个人'] = single
+        oil['两人'] = double
+        return oil
 
 
 #print (translator().stored_2_handler()[1])
+print (handler().oil_each('甲', translator().stored_2_handler()[1]))
+print (handler().product_each('甲', translator().stored_2_handler()[1]))
 print (handler().miles_each('已', translator().stored_2_handler()[1]))
 
 
--- util.py
+++ util.py
@@ -8,6 +8,15 @@ production_value_pre_path = 'production_value'
 #weight of car
 heavy = 33
 
+#coefficient
+light_coe = 0.1
+heavy_coe = 0.2
+very_heavy_coe = 0.3
+
+#Commission of product_value
+single_commission = 0.1
+double_commission = 0.09
+
 def compute_product_value(name, v, round, single, double):
     lstDriver = v[round]['记录一']['司机']
     nDriver = len(lstDriver)
@@ -20,10 +29,10 @@ def compute_product_value(name, v, round, single, double):
 
 
 def compute_miles(name, v, round, single, double):
-    compute_miles_util(name, v, round, single, double, '记录一')
-    compute_miles_util(name, v, round, single, double, '记录二')
+    compute_miles_each_record(name, v, round, single, double, '记录一')
+    compute_miles_each_record(name, v, round, single, double, '记录二')
 
-def compute_miles_util(name, v, round, single, double, record):
+def compute_miles_each_record(name, v, round, single, double, record):
     lstDriver = v[round][record]['司机']
     nDriver = len(lstDriver)
     if name in lstDriver:
@@ -40,11 +49,39 @@ def weight_rules(weight):
     else:
         return '超重车'
 
-def heavy_dict():
+#compute oil
+def comput_oil(name, v, round, single, double):
+    comput_oil_each_record(name, v, round, single, double, '记录一')
+    comput_oil_each_record(name, v, round, single, double, '记录二')
+
+def comput_oil_each_record(name, v, round, single, double, record):
+    lstDriver = v[round][record]['司机']
+    nDriver = len(lstDriver)
+    if name in lstDriver:
+        if nDriver == 1:
+            single[0] += int(v[round][record]['加油'])
+        elif nDriver == 2:
+            double[0] += int(v[round][record]['加油'])
+
+
+def heavy_dict_producer():
     dict = {}
     dict['轻车'] = 0
     dict['重车'] = 0
     dict['超重车'] = 0
     return dict
+
+def record_array_producer():
+    single = []
+    single.append(0)
+    double = []
+    double.append(0)
+    return single, double
+
+def iter(fun, data, name, single, double):
+    for v in data.values():
+        fun(name, v, '第一趟', single, double)
+        if v.get('回头货'):
+            fun(name, v, '回头货', single, double)
 
 
