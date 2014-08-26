af3244b6ed31d6bf85c9c750cdf1cd9128ab52f8
xiejiati <xiejiati2070@163.com>
2014-08-26 12:56:30 +0800

    dd

--- util.py
+++ util.py
@@ -1,28 +1,7 @@
 #coding:utf-8
 __author__ = 'xjt'
 
-#paths
-production_value_pre_path = 'production_value'
 
-
-#weight of car
-heavy = 33
-
-#coefficient
-oil_per_mile_by_weight_coe = {}
-oil_per_mile_by_weight_coe['轻车'] = 4
-oil_per_mile_by_weight_coe['重车'] = 5
-oil_per_mile_by_weight_coe['超重车'] = 6
-
-money_per_liter = 7
-
-#Commission of _product_value
-single_commission = 0.1
-double_commission = 0.09
-
-#tel charge and social security
-money_per_dayoff  = 17
-tel_subside = 100
 
 def compute_product_value(name, v, round, single, double):
     lstDriver = v[round]['记录一']['司机']
